6d22b266c3a57c91fe6bc9f5e27ae62e2bcc08dc
AlexisPark0302 <park_ale000@ephrataschools.org>
2022-01-10 06:15:36 +0000

    Modified CodespacesTest.java

---
+++ studentWork/CodespacesBeta.java
@@ -0,0 +1,9 @@
+package studentWork;
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "Its a new year";
+        System.out.println(x);
+        
+    }
+}
---
+++ studentWork/GitHubCodespacesTest/CodespacesBeta.java
@@ -0,0 +1,9 @@
+package studentWork.GitHubCodespacesTest;
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "Its a new year!!";
+        System.out.println(x);
+        
+    }
+}
